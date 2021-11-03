2a716a827b782f689299ad3debe562a9864f22ed
MrSzpachla <kamilpaszkiet@gmail.com>
2021-11-03 14:35:18 +0100

    end

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,9 @@ public class Main {
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
 
+        double[] tab = {25.32, 84.22, 64.55, 78, 91.1};
+        for (int i = 0; i < tab.length; i++)
+            System.out.print(tab[i] + " ");
 
     }
 }
