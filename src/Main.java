095325162ccfc47db52885a9ab93a87bb4b44a68
MrSzpachla <kamilpaszkiet@gmail.com>
2021-11-03 14:27:21 +0100

    end

--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,28 @@ public class Main {
          * w innym wypadku "Niepodzielny przez 3" użyj kontrukcji if else
          * */
 
+        int wiek = 24;
+        if (wiek % 3 == 0) {
+            System.out.println("Podzielny przez 3");
+        } else {
+            System.out.println("Niepodzielny przez 3");
+        }
 
         /* zad.2 użyj parametru trójargumentowego aby sprawdzić parzystośc nr. indeksu */
 
+        int index = 49750;
+        int parz = (index % 2 == 0) ? 0 : 1;
+        if (parz == 0) System.out.println("Index jest parzysty\n");
+        if (parz == 1) System.out.println("Index nie jest parzysty\n");
 
         /* zad.3 wpisz z klawiatury liczbę zmiennoprzecinkową (użyj klasy Scanner z poprzednich ćwiczeń)
          * i stwórz konstrukcję else if z dowolnymi komunikatami */
 
-
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Podaj ile masz kilometrów do celu: ");
+        int samochod = scan.nextInt();
+        if (samochod > 100) System.out.println("Musisz zatankować! Nie starczy Ci paliwa aby dojechać do celu!\n");
+        else if (samochod == 100) System.out.println("Masz odpowiednią ilość paliwa jak na tą trasę!\n");
+        else if (samochod < 100) System.out.println("Możesz spokojnie jechać, paliwa jest w nadmiarze! \n");
     }
 }
