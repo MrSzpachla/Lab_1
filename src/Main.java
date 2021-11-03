78c27a9bc703b7af0496b00b1270d122f9b44752
MrSzpachla <kamilpaszkiet@gmail.com>
2021-11-03 13:14:58 +0100

    ta

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
-
+        System.out.println("HEJ!");
+        System.out.println("Langusta" + " " + "żywi się wyłącznie owocami morza,");
+        System.out.printf("choć %s mogła, \n", "gdyby");
+        System.out.print("jadłaby dżem");
     }
 }
