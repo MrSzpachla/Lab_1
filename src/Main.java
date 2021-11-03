7ce03bdadab7e346da9f007e07b05d495f1e67ff
MrSzpachla <kamilpaszkiet@gmail.com>
2021-11-03 14:11:14 +0100

    end

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,21 @@ public class Main {
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
 
+        boolean a = true;
+        boolean b = false;
+        boolean c = false;
 
+        System.out.println("!((a && !c) || (!a || b) && (c || !b))) : " + !((a && !c) || (!a || b) && (c || !b)));
+        System.out.println("((a && !b) && (!c || !b)) : " + ((a && !b) && (!c || !b)));
+
+        System.out.println("\n");
+
+        int x = 3;
+        int y = 14;
+        int z = 32;
+
+        System.out.println("((x >= y) || (y != z)) : " + ((x >= y) || (y != z)));
+        System.out.println("((y == z) || (y < z) && (x != z)) : " + ((y == z) || (y < z) && (x != z)));
+        System.out.println("((y != x) && (!a || c) && (!c || !b) && (x < z)) : " + ((y != x) && (!a || c) && (!c || !b) && (x < z)));
     }
 }
